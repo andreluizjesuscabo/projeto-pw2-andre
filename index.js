cd01d3aba5305b1ed383d2ca8b99652f7aebd396
SuperJigsaw <andrecabo8@gmail.com>
2023-03-27 22:12:43 -0300

    Adicição de routes

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require ('express');
 
 const app = express();
 
+app.get('/teste', (req, res)=>{
+    res.send('TESTE!!!');
+});
+
 app.listen(3000, ()=>{
     console.log('Servidor HTTP esta rodando lindamente em http://localhost:3000')
 });
